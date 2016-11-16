64c17e82a3c992c76692964df0b2e158bdc41aee
Alka <Alkanoor@github.com>
2016-11-16 20:49:19 +0100

    en cours

--- genetic/include/common_interpretations.hpp
+++ genetic/include/common_interpretations.hpp
@@ -10,12 +10,10 @@ class Common_Interpretations
 {
     public:
         template <typename T> // T is either an int or a float
+        static void add_interpretation(const std::string& name, const std::function<T(const std::bitset<N>&, int, int)>& func);
+
+        template <typename T> // T is either an int or a float
         static const std::function<T(const std::bitset<N>&, int, int)>& get_interpretation(const std::string& name);
-
-        static int binary_interpretation(const std::bitset<N>&, int, int);
-        static int gray_interpretation(const std::bitset<N>&, int, int);
-
-        static float log_interpretation(const std::bitset<N>&, int, int);
 
         static void init();
 
@@ -27,29 +25,19 @@ class Common_Interpretations
 };
 
 
+template <>
+const std::function<int(const std::bitset<N>&, int, int)>& Common_Interpretations<N>::get_interpretation(const std::string& name)
+{return integer_interpretations[name];}
+
+template <>
+const std::function<float(const std::bitset<N>&, int, int)>& Common_Interpretations<N>::get_interpretation(const std::string& name)
+{return float_interpretations[name];}
+
 template <size_t N>
-Common_Interpretations<N>::
-
-
-const std::function<T(const std::bitset<N>&, int, int)>& Common_Interpretations<N>::get_interpretation(const std::string& name);
-
-int Common_Interpretations<N>::binary_interpretation(const std::bitset<N>& adn, int min, int max)
-{}
-
-int Common_Interpretations<N>::gray_interpretation(int Common_Interpretations<N>::binary_interpretation(const std::bitset<N>& adn, int min, int max)
-{}
-
-float Common_Interpretations<N>::log_interpretation(const std::bitset<N>& adn, int min, int max)
-{
-    float res = 0;
-    for(int i=min; i<max; i++)
-        if(adn[i])
-            res += log(i-min+1);
-    return res;
-}
-
 void Common_Interpretations<N>::init()
 {
+    initialized = true;
+    
     integer_interpretations["BINARY"] = std::bind(&Common_Interpretations<N>::binary_interpretation, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
     integer_interpretations["GRAY"] = std::bind(&Common_Interpretations<N>::gray_interpretation, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
 
